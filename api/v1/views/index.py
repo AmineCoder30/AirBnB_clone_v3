375f594799a22b1279ea5a48f3203a79a75f333b
AmineCoder30 <code30a@gmail.com>
2023-10-29 13:00:52 -0700

    fix the index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
-""" Index """
+"""define routes of blueprint
+"""
+
 from api.v1.views import app_views
-from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status", strict_slashes=False, methods=["GET"])
 def status():
-    """ Returns JSON """
-    return jsonify(status="OK")
+    return {
+        "status": "OK",
+    }
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route("/stats", strict_slashes=False, methods=["GET"])
 def stats():
-    """ Returns the number of each instance type """
-    return jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    return {
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users,
+    }
